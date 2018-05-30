5c46cf59e34c66fcbd05f37951b7f63d6ae5c431
shahy <shahy@DESKTOP-NELADPE>
2018-05-30 23:19:11 +0530

    banking application

--- banking/src/banking/Banking.java
+++ banking/src/banking/Banking.java
@@ -4,97 +4,103 @@ class operation
 {
     Scanner inp=new Scanner(System.in);
     String name;
-    int accno;
-    int bal;
+    int account_no;
+    int balance;
     void create()
     {
-        
         System.out.println("Enter the account no::");
-        accno=inp.nextInt();
+        account_no=inp.nextInt();
         System.out.println("Enter the name of the account holder::");
         name=inp.next();
-        
-        
     }
     void display()
     {
-       System.out.println("Name::"+name+"\nAccount no::"+accno);
-    }
-    boolean search(int no)
-    {
-        display();
-        if(accno==no)
-        {
-            return(true);
-        }
-        return false;
+       System.out.println("Name::"+name+"\nAccount no::"+account_no);
     }
     void deposit()
     {
         int amt;
         System.out.println("Enter the amount to deposit::");
         amt=inp.nextInt();
-        bal=bal+amt;
+        balance=balance+amt;
+        System.out.println("balance="+balance);
     }
     void withdraw()
     {
         int amt;
         System.out.println("Enter the amount to withdraw::");
         amt=inp.nextInt();
-        if(bal>amt)
-            bal=bal-amt;
+        if(balance>amt)
+            balance=balance-amt;
         else
             System.out.println("Insufficient balance::");
+        System.out.println("balanceance="+balance);
     }
 }
 public class Banking
 {
     public static void main(String[] args)
     {
-        int ch,i,no;
         Scanner inp=new Scanner(System.in);
-        String name;
-        int n,bal=0;
+        int choice,i,accno,amt,sum=0,max,j,acc_withdraw,acc_deposit;
         int a[]=new int[10];
-            operation[] obj=new operation[4];
+        boolean found4=false;
+        operation[] obj=new operation[4];
         for(i=0;i<obj.length;i++)
         {
             obj[i]=new operation();
-            obj[i].create();
         }
-   
-      
-        n=0;
+        i=0;
         do
         {
-            System.out.println("Enter 1.Add account\n2.Deposit money\n3.Withdraw money\n4.Transfer money into other account\n5.Total amount of money in the bank\n6.Richest person in the bank.");
-            ch=inp.nextInt();
-            switch(ch)
+            System.out.println("Enter 1.Add account\n2.Deposit money\n3.Withdraw money\n4.Transfer money into other account\n5.Total amount of money in the bank\n6.Richoiceest person in the bank.");
+            choice=inp.nextInt();
+            switch(choice)
             {
                 case 1:
-                    for(i=0;i<obj.length;i++)
+                    obj[i].create();
+                    for(j=0;j<i;j++)
                     {
-                        obj[i].display();
+                        if(obj[j].account_no==obj[i].account_no)
+                            found4=true;
                     }
+                    if(found4)
+                    {
+                        System.out.println("Account no already exist Please re-enter the account no::");
+                        obj[i].create();
+                    }
+                    else
+                         obj[i].display();
+                    i++;
+                    break;
                 case 2:System.out.println("Enter the account no::");
-                        no=inp.nextInt();
+                        accno=inp.nextInt();
                         boolean found=false;
                         for(i=0;i<obj.length;i++)
                         {
-                            found=obj[i].search(no);
-                        }
-                        if(found)
-                        {
-                            obj[i].deposit();
-                        }
-                        else
+                            if(obj[i].account_no==accno)
+                            {
+                                 found=true;
+                                 break;
+                            }
+                        }                        
+                        if(!found)
                             System.out.println("Account doesnot exist");
+                        else
+                        {                            
+                            obj[i].deposit(); 
+                        }
+                        break;
                 case 3:System.out.println("Enter the account no::");
-                        no=inp.nextInt();
+                        accno=inp.nextInt();
                         boolean found1=false;
                         for(i=0;i<obj.length;i++)
                         {
-                            found1=obj[i].search(no);
+                             if(obj[i].account_no==accno)
+                            {
+                                 found1=true;
+                                 break;
+                            }
                         }
                         if(found1)
                         {
@@ -102,11 +108,53 @@ public class Banking
                         }
                         else
                             System.out.println("Account doesnot exist");
-                      
-                        
-                        
+                        break;
+                case 4:System.out.println("Enter the account no's from and to tranfer it::");
+                        acc_withdraw=inp.nextInt();
+                        acc_deposit=inp.nextInt();
+                        boolean found3=false;
+                        for(i=0;i<obj.length;i++)
+                        {
+                            for(j=0;j<obj.length;j++)
+                            {
+                                if(obj[i].account_no==acc_withdraw&&obj[j].account_no==acc_deposit)
+                                {
+                                   System.out.println("Enter the amount to withdraw::");
+                                    amt=inp.nextInt();
+                                    if(obj[i].balance>amt)
+                                    {
+                                         obj[i].balance-=amt;
+                                         obj[j].balance+=amt;
+                                         System.out.println("balance of account that transferred money is::"+obj[i].balance);
+                                         System.out.println("balance of account that recieved money is::"+obj[j].balance);
+                                    }
+                                    
+                                }
+                            }
+                        }
+                        if(!found3)
+                        {
+                            System.out.println("Wrong account number entered!!!");
+                        }
+                        break;
+                case 5: System.out.println("Total amount of money in the bank is::");   
+                        for(i=0;i<obj.length;i++)
+                        {
+                            sum+=obj[i].balance;
+                        }
+                        System.out.println(sum);
+                        break;
+                case 6:max=0;
+                        for(j=1;j<obj.length;j++)
+                        {
+                            if(obj[j].balance>obj[max].balance)
+                            {
+                                max=j;
+                            }
+                        }
+                    System.out.println("The richest person in the bank is::"+obj[max].name+" whose account number is::"+obj[max].account_no);
+                    break;                   
             }
-        }while(ch!=4);
+        }while(choice!=7);
     }
-    
 }
